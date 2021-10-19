e4748fb3b1dc7b41e5fc6185825825f412d77b78
Yonathan Avila <50575930+yonathanavila@users.noreply.github.com>
2021-10-19 16:55:41 -0600

    Repositorio de Prueba

---
+++ models/server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+
+class Server {
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT;
+
+    // Middlewares
+    // Rutas de mi aplicación
+    this.middlewares();
+
+    this.routes();
+  }
+
+  middlewares() {
+    //Directorio Publico
+    this.app.use(express.static("public"));
+  }
+
+  routes() {
+    this.app.get("/api", function (req, res) {
+      res.send("Hello World");
+    });
+  }
+
+  listen() {
+    this.app.listen(process.env.PORT, () => {
+      console.log("Servidor corriendo en puerto", this.port);
+    });
+  }
+}
+
+module.exports = Server;
